c94a0ce94ba65ac2f37d25d5c2e6dde4eb7711d2
GrandeRojo17 <38540605+GrandeRojo17@users.noreply.github.com>
2020-06-21 13:28:09 -0500

    2/3 Went through and added the correct routes ready for test and readMe and github to be updated

---
+++ db/store.js
@@ -0,0 +1,48 @@
+const util = require("util");
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+  read() {
+    return readFileAsync("../db/db.json", "utf8");
+  }
+  write(note) {
+    return writeFileAsync("../db/db.json", JSON.stringify(note));
+  }
+
+  getNotes() {
+    return this.read().then((notes) => {
+      let parsedNotes;
+
+      try {
+        parsedNotes = [].concat(JSON.parse(notes));
+      } catch (error) {
+        parsedNotes = [];
+      }
+      return parsedNotes;
+    });
+  }
+
+  addNote(note) {
+    const { title, text } = note;
+    var id = 100;
+
+    if (!title || !text) {
+      throw new Error("Notes must have title & text ");
+    }
+    const newNote = { title, text, id: id++ };
+
+    return this.getNotes()
+      .then((notes) => {
+        [...notes, newNote];
+      })
+      .then((updatedNotes) => this.write(updatedNotes))
+      .then(() => newNote);
+  }
+  deleteNote(id) {
+    return this.getNotes()
+      .then((notes) => notes.filter((note) => note.id !== id))
+      .then((filteredNotes) => this.write(filteredNotes));
+  }
+}
+module.exports = new Store();
