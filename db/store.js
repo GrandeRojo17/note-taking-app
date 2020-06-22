99eb83202c5dc1be12d0bb9fba9b3b0194d00e34
GrandeRojo17 <38540605+GrandeRojo17@users.noreply.github.com>
2020-06-22 08:52:43 -0500

    wonderful

--- db/store.js
+++ db/store.js
@@ -1,5 +1,9 @@
 const util = require("util");
 const fs = require("fs");
+
+// This package will be used to generate our unique ids. https://www.npmjs.com/package/uuid
+const uuidv1 = require("uuid/v1");
+
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -7,6 +11,7 @@ class Store {
   read() {
     return readFileAsync("db/db.json", "utf8");
   }
+
   write(note) {
     return writeFileAsync("db/db.json", JSON.stringify(note));
   }
@@ -15,33 +20,40 @@ class Store {
     return this.read().then((notes) => {
       let parsedNotes;
 
+      // If notes isn't an array or can't be turned into one, send back a new empty array
       try {
         parsedNotes = [].concat(JSON.parse(notes));
-      } catch (error) {
+      } catch (err) {
         parsedNotes = [];
       }
+
       return parsedNotes;
     });
   }
 
   addNote(note) {
     const { title, text } = note;
-    var id = 100;
 
     if (!title || !text) {
-      throw new Error("Notes must have title & text ");
+      throw new Error("Note 'title' and 'text' cannot be blank");
     }
-    const newNote = { title, text, id: id++ };
 
+    // Add a unique id to the note using uuid package
+    const newNote = { title, text, id: uuidv1() };
+
+    // Get all notes, add the new note, write all the updated notes, return the newNote
     return this.getNotes()
       .then((notes) => [...notes, newNote])
       .then((updatedNotes) => this.write(updatedNotes))
       .then(() => newNote);
   }
+
   removeNote(id) {
+    // Get all notes, remove the note with the given id, write the filtered notes
     return this.getNotes()
       .then((notes) => notes.filter((note) => note.id !== id))
       .then((filteredNotes) => this.write(filteredNotes));
   }
 }
+
 module.exports = new Store();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
-const store = require("../db/store");
 const router = require("express").Router();
+const store = require("../db/store");
 
+// GET "/api/notes" responds with all notes from the database
 router.get("/notes", (req, res) => {
   store
     .getNotes()
@@ -14,10 +15,13 @@ router.post("/notes", (req, res) => {
     .then((note) => res.json(note))
     .catch((err) => res.status(500).json(err));
 });
+
+// DELETE "/api/notes" deletes the note with an id equal to req.params.id
 router.delete("/notes/:id", (req, res) => {
   store
     .removeNote(req.params.id)
     .then(() => res.json({ ok: true }))
     .catch((err) => res.status(500).json(err));
 });
+
 module.exports = router;
